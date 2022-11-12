3e16f7cfdf369c5bdbd54fcf2ea812e13707092c
Dennie Bastiaan <dennie@tellick.nl>
2022-11-13 00:24:42 +0100

    Initial

---
+++ Form1.cs
@@ -0,0 +1,120 @@
+using System.Timers;
+
+namespace _3drender;
+
+public class Vec3 {
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
+
+public class LineIdx {
+    public int A { get; set; }
+    public int B { get; set; }
+}
+
+public partial class Form1 : Form
+{
+    private readonly float _focalLength = 5;
+
+    private readonly List<Vec3> _cube = new() 
+    {
+        new Vec3() { X = -1, Y = -1, Z = -1 },
+        new Vec3() { X = 1, Y = -1, Z = -1 },
+        new Vec3() { X = 1, Y = 1, Z = -1 },
+        new Vec3() { X = -1, Y = 1, Z = -1 },
+        new Vec3() { X = -1, Y = -1, Z = 1 },
+        new Vec3() { X = 1, Y = -1, Z = 1 },
+        new Vec3() { X = 1, Y = 1, Z = 1 },
+        new Vec3() { X = -1, Y = 1, Z = 1 },
+    };
+
+    private readonly List<LineIdx> _lines = new() 
+    {
+        new LineIdx() { A = 0, B = 1 },
+        new LineIdx() { A = 1, B = 2 },
+        new LineIdx() { A = 2, B = 3 },
+        new LineIdx() { A = 3, B = 0 },
+        new LineIdx() { A = 0, B = 4 },
+        new LineIdx() { A = 1, B = 5 },
+        new LineIdx() { A = 2, B = 6 },
+        new LineIdx() { A = 3, B = 7 },
+        new LineIdx() { A = 4, B = 5 },
+        new LineIdx() { A = 5, B = 6 },
+        new LineIdx() { A = 6, B = 7 },
+        new LineIdx() { A = 7, B = 4 },
+    };
+
+    public Form1()
+    {
+        InitializeComponent();
+        
+        Width = 1280;
+        Height = 720;
+        DoubleBuffered = true;
+
+        var timer = new System.Timers.Timer();
+        timer.Elapsed += new ElapsedEventHandler(TimerEvent);
+        timer.Interval = 16;
+        timer.Start();
+    }
+
+    private void TimerEvent(object source, ElapsedEventArgs e) 
+    {
+        foreach (var vertex in _cube)
+        {
+            RotateY(vertex, 0.01);
+        }
+        
+        Refresh();
+    }
+
+    private static void RotateY(Vec3 vertex, double rotation)
+    {
+        double a = Math.Atan(vertex.Z / vertex.X);
+        double s = vertex.Z / Math.Sin(a);
+
+        a += rotation;
+
+        double x = Math.Cos(a) * s;
+        double z = Math.Sin(a) * s;
+
+        vertex.X = (float)x;
+        vertex.Z = (float)z;
+    }
+
+    private float ProjectX(Vec3 position) 
+    {
+        return _focalLength * position.X / (_focalLength + position.Z) * 160 + (Width / 2);
+    }
+
+    private float ProjectY(Vec3 position) 
+    {
+        return _focalLength * position.Y / (_focalLength + position.Z) * 160 + (Height / 2);
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        e.Graphics.Clear(Color.White);
+
+        foreach (var vertex in _cube)
+        {
+            float projectedX = ProjectX(vertex);
+            float projectedY = ProjectY(vertex);
+
+            e.Graphics.FillRectangle(Brushes.Black, projectedX - 4.0f, projectedY - 4.0f, 8.0f, 8.0f);    
+        }
+
+        foreach (var line in _lines)
+        {
+            float projectedXA = ProjectX(_cube[line.A]);
+            float projectedYA = ProjectY(_cube[line.A]);
+            float projectedXB = ProjectX(_cube[line.B]);
+            float projectedYB = ProjectY(_cube[line.B]);
+
+            e.Graphics.DrawLine(Pens.Blue, projectedXA, projectedYA, projectedXB, projectedYB);
+        }
+    }
+}
