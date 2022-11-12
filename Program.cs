7da9cb2ad7c312e88e12d28c6d090a19424a05c5
Dennie Bastiaan <dennie@tellick.nl>
2022-11-13 00:45:41 +0100

    more cleanup

--- Program.cs
+++ Program.cs
@@ -8,13 +8,9 @@ static class Program
     [STAThread]
     static void Main()
     {
-        AllocConsole();
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new RenderCanvas());
-    }    
-
-    [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-      private static extern bool AllocConsole();
+    }
 }
